897f4e2d48a30ddc8f663cc0396f752c12e8eecc
Ayush02jain <ayushjsan01@gmail.com>
2026-04-04 23:07:38 +0530

    again.........

--- components/portfolio/hero.tsx
+++ components/portfolio/hero.tsx
@@ -338,7 +338,7 @@ export function Hero() {
                 className="w-72 h-72 sm:w-80 sm:h-80 lg:w-96 lg:h-96 rounded-2xl overflow-hidden border-[3px] border-[#A78BFA]/60"
               >
                 <img
-                  src="/profile.jpg"
+                  src="/ayush.JPG.jpeg"
                   alt="Ayush Jain"
                   className="w-full h-full object-cover object-top"
                 />
